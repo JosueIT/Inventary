ff7286d7720dfe279c699fd0fd7b7926a3e2f4d7
JosueIT <72401872+JosueIT@users.noreply.github.com>
2021-08-17 17:15:12 -0500

    Init

---
+++ Controllers/ComputersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Data;
+
+namespace INVQTM.Controllers
+{
+    public class ComputersController : Controller
+    {
+        public IActionResult Index(string serial)
+        {
+            return PartialView();
+        }
+
+        public IActionResult List()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AddForm()
+        {
+            return RedirectToAction("AddForm", "Home");
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult AssingForm(int id)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Assing()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string serial)
+        {
+            return PartialView();
+        }
+
+        public ActionResult Filter(int estado)
+        {
+            return PartialView();
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/Scripts.js
@@ -0,0 +1,11 @@
+﻿$("#lapt").click(function () {
+    $.ajax({
+        type: "GET",
+        url: "/Computers/Index",
+    }).done(function (data) {
+        $("section").empty();
+        $("section").append(data);
+    }).fail(function () {
+        alert("error");
+    })
+});
